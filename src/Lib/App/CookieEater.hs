908ea8eca3ed21d38aa44b155275a3340ca6edb1
Tomas Korcak <korczis@gmail.com>
2014-08-01 22:47:56 -0700

    Split to more parts

--- src/Lib/App/CookieEater.hs
+++ src/Lib/App/CookieEater.hs
@@ -2,13 +2,16 @@ module App.CookieEater where
 
 import qualified Cli.Cli as Cli
 
+-- Checks if user passed right cookie
 checkCookie :: String -> Bool
 checkCookie x = x == "cookie"
 
+-- Congratulates user
 done :: IO ()
 done = do
-	Cli.say "Done"
+	Cli.say "Thank you"
 
+-- Recursive helper function asking user for cookie
 ask :: IO ()
 ask = do
 	-- Ask user for cookie
@@ -17,14 +20,24 @@ ask = do
 	-- Store user's input in cookie variable
 	cookie <- getLine
 
-	-- If user found the cookie print "Thanks" then
-	if checkCookie cookie then 
-		Cli.say "Thanks" 
-	-- IF not ask user again
-	else 
-		Cli.say "Not a cookie I wanted";
-		ask
-	
+	check checkCookie cookie done ask
+
+-- Use use function specified as first param for checking if second param is true.
+-- If so, call function which is specified as third param else call function specified as fourth param.
+check :: (String -> Bool) -> String -> IO () -> IO () -> IO ()
+check f x a b = if f x then a else b
+
+-- One tick of application loop
+tick :: IO ()
+tick = do
+	ask
+
+-- Application loop
+loop :: IO ()
+loop = do
+	tick
+
+-- Application entry point
 run :: IO ()
 run = do
-	ask
+	loop
