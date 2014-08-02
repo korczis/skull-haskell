ab32180e819f4eb75341d9760415af701d3c0fcc
Tomas Korcak <korczis@gmail.com>
2014-08-01 22:14:05 -0700

    More comments

--- src/Lib/App/CookieEater.hs
+++ src/Lib/App/CookieEater.hs
@@ -11,9 +11,19 @@ done = do
 
 ask :: IO ()
 ask = do
+	-- Ask user for cookie
 	Cli.say "Give me a cookie"
+
+	-- Store user's input in cookie variable
 	cookie <- getLine
-	if checkCookie cookie then Cli.say "Thanks" else ask
+
+	-- If user found the cookie print "Thanks" then
+	if checkCookie cookie then 
+		Cli.say "Thanks" 
+	-- IF not ask user again
+	else 
+		Cli.say "Not a cookie I wanted";
+		ask
 	
 run :: IO ()
 run = do
